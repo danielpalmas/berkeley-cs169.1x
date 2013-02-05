63fd9e56c5670c53541ccf2255234a64af72a6b2
Simon COURTOIS <scourtois@cubyx.fr>
2013-02-05 17:09:34 +0100

    hw3

--- hw3/features/step_definitions/movie_steps.rb
+++ hw3/features/step_definitions/movie_steps.rb
@@ -1,28 +1,48 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
-  movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
-  end
-  flunk "Unimplemented"
+  Movie.create(movies_table.hashes)
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.should match(/#{e1}.*#{e2}/m)
+end
+
+Then /I should see no movie/ do
+  Movie.find_each do |movie|
+    page.should_not have_content(movie.title)
+  end
+end
+
+Then /I should see all of the movies/ do
+  Movie.find_each do |movie|
+    page.should have_content(movie.title)
+  end
+end
+
+Then /I should see movies with the following ratings: (.*)/ do |rating_list|
+  ratings = rating_list.split(/\s*,\s*/)
+  Movie.where(rating: ratings).find_each do |movie|
+    page.should have_content(movie.title)
+  end
+end
+
+Then /I should not see movies with the following ratings: (.*)/ do |rating_list|
+  ratings = rating_list.split(/\s*,\s*/)
+  Movie.where(rating: ratings).find_each do |movie|
+    page.should_not have_content(movie.title)
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+When /I ((?:un)?check) the following ratings: (.*)/ do |check, rating_list|
+  rating_list.split(/\s*,\s*/).each do |rating|
+    step %Q{I #{check} "ratings_#{rating}"}
+  end
 end
